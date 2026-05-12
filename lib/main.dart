9bc4de3b48e802870f4b5b5e9d15a594f712c1b6
alisher3336 <erzhanovalisher4@gmail.com>
2026-05-12 11:13:57 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -133,3 +133,29 @@ class ArticleWidget extends StatelessWidget {
     );
   }
 }
+
+class ArticlePage extends StatelessWidget {
+  final Summary summary;
+  final VoidCallback nextArticle;
+
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(
+            onPressed: nextArticle,
+            child: const Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
